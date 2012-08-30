168960f573f3aaa2c7bd35a2f369eb5cec8f022a
David Hagan <david.hagan@monash.edu>
2012-08-30 02:43:19 +0000

    zipstream safetying

--- MeTL2011/MeTL2011/src/main/scala/com/metl/MeTL2011History.scala
+++ MeTL2011/MeTL2011/src/main/scala/com/metl/MeTL2011History.scala
@@ -18,10 +18,16 @@ class MeTL2011History(serverName:String,http:HttpProvider) extends HistoryRetrie
 		val stream = new ByteArrayInputStream(downloadedBytes)
 		val zipStream = new java.util.zip.ZipInputStream(stream)
 		def parseMessages(inputStream:ZipInputStream):List[MeTLStanza] = {
+			try {
 			inputStream.getNextEntry match {
 				case ze:ZipEntry if ze.getName.endsWith(".xml") => tryo(dailyXmlToListOfStanzas(xml.XML.loadString(IOUtils.toString(inputStream)+"</logCollection>"))).openOr(List.empty[MeTLStanza]) ::: parseMessages(inputStream)
 				case ze:ZipEntry => parseMessages(inputStream)
 				case _ => List.empty[MeTLStanza]
+			}
+			} catch {
+				case e:Throwable => {
+					List.empty[MeTLStanza]
+				}
 			}
 		}
 		val messages = Stopwatch.time("MeTL2011History.getMeTLHistory.unzipAndParse", () => parseMessages(zipStream).toList.sortBy(s => s.timestamp))
