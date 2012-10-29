dd0768466797260f943b76d75386bb9dab5705ce
Joshua Jordan <joshua.jordan@monash.edu>
2012-10-29 01:06:05 +0000

    Tests for XmlUtils

--- MeTLData/MeTLData/src/test/scala/com/metl/data/GenericXmlSerializerSuite.scala
+++ MeTLData/MeTLData/src/test/scala/com/metl/data/GenericXmlSerializerSuite.scala
@@ -46,4 +46,178 @@ class GenericXmlSerializerSuite extends FunSuite with MockitoSugar {
         assert(result === Privacy.NOT_SET) 
     }
 
+    test("extract color as hexstring from specified element name") {
+        val content = <ink><color>#0fafbfcf</color><privacy>private</privacy></ink>
+        val result = XmlUtils.getColorByName(content, "color")
+
+        assert(result === Color(0x0f, 0xaf, 0xbf, 0xcf))
+    }
+
+    test("extract color as long hexstring from specified element name") {
+        val content = <ink><color>#0fafbfcf7b</color><privacy>private</privacy></ink>
+        val result = XmlUtils.getColorByName(content, "color")
+
+        assert(result === Color.default)
+    }
+
+    test("extract color as 3 numbers from content") {
+        val content = <ink><blah><color>240 250 128</color></blah><privacy>private</privacy></ink>
+        val result = XmlUtils.getColorByName(content, "color")
+
+        assert(result === Color(0xff, 240, 250, 128))
+    }
+
+    test("extract color as 4 numbers from content") {
+        val content = <ink><blah><color>240 250 128 120</color></blah><privacy>private</privacy></ink>
+        val result = XmlUtils.getColorByName(content, "color")
+
+        assert(result === Color(120, 240, 250, 128))
+    }
+
+    test("extract non-existant color element from content") {
+        val content = <ink><elephant>african</elephant><privacy>private</privacy></ink>
+        val result = XmlUtils.getColorByName(content, "color")
+
+        assert(result === Color.default)
+    }
+
+    test("extract invalid color from content") {
+        val content = <ink><color>mango</color><privacy>private</privacy></ink>
+        val result = XmlUtils.getColorByName(content, "color")
+
+        assert(result === Color.default)
+    }
+
+    test("extract 4 number color outside of range") {
+        val content = <ink><blah><color>240 400 128 120</color></blah><privacy>private</privacy></ink>
+        val result = XmlUtils.getColorByName(content, "color")
+
+        assert(result === Color(120, 240, 255, 128))
+    }
+
+    test("extract 3 number color outside of range") {
+        val content = <ink><blah><color>240 400 128</color></blah><privacy>private</privacy></ink>
+        val result = XmlUtils.getColorByName(content, "color")
+
+        assert(result === Color(255, 240, 255, 128))
+    }
+
+    test("extract string value from specified element") {
+        val content = <ink><elephant>african</elephant><privacy>private</privacy></ink>
+        val result = XmlUtils.getStringByName(content, "elephant")
+
+        assert(result === "african")
+    }
+
+    test("extract string value from non-existant specified element") {
+        val content = <ink><elephant>african</elephant><privacy>private</privacy></ink>
+        val result = XmlUtils.getStringByName(content, "tree")
+
+        assert(result === "")
+    }
+
+    test("extract true boolean value from specified element") {
+        val content = <ink><isHighlighter>true</isHighlighter><privacy>private</privacy></ink>
+        val result = XmlUtils.getBooleanByName(content, "isHighlighter")
+
+        assert(result === true)
+    }
+
+    test("extract TRUE boolean value from specified element") {
+        val content = <ink><isHighlighter>TRUE</isHighlighter><privacy>private</privacy></ink>
+        val result = XmlUtils.getBooleanByName(content, "isHighlighter")
+
+        assert(result === true)
+    }
+
+    test("extract TrUe boolean value from specified element") {
+        val content = <ink><isHighlighter>TrUe</isHighlighter><privacy>private</privacy></ink>
+        val result = XmlUtils.getBooleanByName(content, "isHighlighter")
+
+        assert(result === true)
+    }
+
+    test("extract false boolean value from specified element") {
+        val content = <ink><isHighlighter>false</isHighlighter><privacy>private</privacy></ink>
+        val result = XmlUtils.getBooleanByName(content, "isHighlighter")
+
+        assert(result === false)
+    }
+
+    test("extract FALSE boolean value from specified element") {
+        val content = <ink><isHighlighter>FALSE</isHighlighter><privacy>private</privacy></ink>
+        val result = XmlUtils.getBooleanByName(content, "isHighlighter")
+
+        assert(result === false)
+    }
+
+    test("extract invalid boolean value from specified element") {
+        val content = <ink><isHighlighter>termites</isHighlighter><privacy>private</privacy></ink>
+        val result = XmlUtils.getBooleanByName(content, "isHighlighter")
+
+        assert(result === false)
+    }
+
+    test("extract negative double value from specified element") {
+        val content = <ink><coordX>-123434.02345</coordX></ink>
+        val result = XmlUtils.getDoubleByName(content, "coordX")
+
+        assert(result === -123434.02345)
+    }
+  
+    test("extract positive double value from specified element") {
+        val content = <ink><coordX>434.045</coordX></ink>
+        val result = XmlUtils.getDoubleByName(content, "coordX")
+
+        assert(result === 434.045)
+    }
+
+    test("extract invalid double value from specified element") {
+        val content = <ink><coordX>pants</coordX></ink>
+        val result = XmlUtils.getDoubleByName(content, "coordX")
+
+        assert(result === -1D)
+    }
+
+    test("extract negative long value from specified element") {
+        val content = <ink><timestamp>-142345224502350203</timestamp></ink>
+        val result = XmlUtils.getLongByName(content, "timestamp")
+
+        assert(result === -142345224502350203L)
+    }
+  
+    test("extract positive long value from specified element") {
+        val content = <ink><timestamp>92387434597823495</timestamp></ink>
+        val result = XmlUtils.getLongByName(content, "timestamp")
+
+        assert(result === 92387434597823495L)
+    }
+
+    test("extract invalid long value from specified element") {
+        val content = <ink><timestamp>pants</timestamp></ink>
+        val result = XmlUtils.getLongByName(content, "timestamp")
+
+        assert(result === -1L)
+    }
+
+    test("extract negative int value from specified element") {
+        val content = <ink><id>-1234235</id></ink>
+        val result = XmlUtils.getIntByName(content, "id")
+
+        assert(result === -1234235)
+    }
+  
+    test("extract positive int value from specified element") {
+        val content = <ink><id>345377</id></ink>
+        val result = XmlUtils.getIntByName(content, "id")
+
+        assert(result === 345377)
+    }
+
+    test("extract invalid int value from specified element") {
+        val content = <ink><id>pants</id></ink>
+        val result = XmlUtils.getIntByName(content, "id")
+
+        assert(result === -1)
+    }
 }
