21686e5ec087844bd28a9f3cc3ec5a64126739ac
Joshua Jordan <joshua.jordan@monash.edu>
2012-11-02 01:13:24 +0000

    Added more tests for xml serializer/deserializer and added property matchers for data types

--- MeTLData/MeTLData/src/test/scala/com/metl/data/GenericXmlSerializerSuite.scala
+++ MeTLData/MeTLData/src/test/scala/com/metl/data/GenericXmlSerializerSuite.scala
@@ -3,15 +3,331 @@ package com.metl.data
 import org.scalatest._
 import org.scalatest.FunSuite
 import org.scalatest.BeforeAndAfter
+import org.scalatest.matchers.{ShouldMatchers, HavePropertyMatcher, HavePropertyMatchResult}
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.OptionValues._
 
 import org.mockito.Mockito._
 import org.mockito.Matchers.{eq => the, any, anyInt}
 
+import net.liftweb.common._
 import scala.xml._
+import Privacy._
 
-class GenericXmlSerializerSuite extends FunSuite with MockitoSugar with BeforeAndAfter {
+trait MeTLDataMatchers {
+
+    def compareBoxedArray[A](left: Box[Array[A]], right: Box[Array[A]]) : Boolean = 
+      (left, right) match {
+          case (Empty, Empty) => true  
+          case (Full(x), Full(y)) => x.sameElements(y)
+          case _ => false
+      }
+
+    def server(expectedValue: ServerConfiguration) =
+      new HavePropertyMatcher[MeTLStanza, ServerConfiguration] {
+        def apply(stanza: MeTLStanza) =
+          HavePropertyMatchResult(
+            stanza.server == expectedValue,
+            "server",
+            expectedValue,
+            stanza.server
+          )
+      }
+
+    def author(expectedValue: String) =
+      new HavePropertyMatcher[MeTLStanza, String] {
+        def apply(stanza: MeTLStanza) =
+          HavePropertyMatchResult(
+            stanza.author == expectedValue,
+            "author",
+            expectedValue,
+            stanza.author
+          )
+      }
+
+    def timestamp(expectedValue: Long) =
+      new HavePropertyMatcher[MeTLStanza, Long] {
+        def apply(stanza: MeTLStanza) =
+          HavePropertyMatchResult(
+            stanza.timestamp == expectedValue,
+            "timestamp",
+            expectedValue,
+            stanza.timestamp
+          )
+      }
+
+    def target(expectedValue: String) =
+      new HavePropertyMatcher[MeTLCanvasContent, String] {
+        def apply(stanza: MeTLCanvasContent) =
+          HavePropertyMatchResult(
+            stanza.target == expectedValue,
+            "target",
+            expectedValue,
+            stanza.target
+          )
+      }
+
+    def privacy(expectedValue: Privacy) =
+      new HavePropertyMatcher[MeTLCanvasContent, Privacy] {
+        def apply(stanza: MeTLCanvasContent) =
+          HavePropertyMatchResult(
+            stanza.privacy == expectedValue,
+            "privacy",
+            expectedValue,
+            stanza.privacy
+          )
+      }
+
+    def slide(expectedValue: String) =
+      new HavePropertyMatcher[MeTLCanvasContent, String] {
+        def apply(stanza: MeTLCanvasContent) =
+          HavePropertyMatchResult(
+            stanza.slide == expectedValue,
+            "slide",
+            expectedValue,
+            stanza.slide
+          )
+      }
+
+    def identity(expectedValue: String) =
+      new HavePropertyMatcher[MeTLCanvasContent, String] {
+        def apply(stanza: MeTLCanvasContent) =
+          HavePropertyMatchResult(
+            stanza.identity == expectedValue,
+            "identity",
+            expectedValue,
+            stanza.identity
+          )
+      }
+
+    def scaleFactorX(expectedValue: Double) =
+      new HavePropertyMatcher[MeTLCanvasContent, Double] {
+        def apply(stanza: MeTLCanvasContent) =
+          HavePropertyMatchResult(
+            stanza.scaleFactorX == expectedValue,
+            "scaleFactorX",
+            expectedValue,
+            stanza.scaleFactorX
+          )
+      }
+
+    def scaleFactorY(expectedValue: Double) =
+      new HavePropertyMatcher[MeTLCanvasContent, Double] {
+        def apply(stanza: MeTLCanvasContent) =
+          HavePropertyMatchResult(
+            stanza.scaleFactorY == expectedValue,
+            "scaleFactorY",
+            expectedValue,
+            stanza.scaleFactorY
+          )
+      }
+
+    def tag(expectedValue: String) =
+      new HavePropertyMatcher[MeTLImage, String] {
+        def apply(stanza: MeTLImage) =
+          HavePropertyMatchResult(
+            stanza.tag == expectedValue,
+            "tag",
+            expectedValue,
+            stanza.tag
+          )
+      }
+
+    def source(expectedValue: Box[String]) =
+      new HavePropertyMatcher[MeTLImage, Box[String]] {
+        def apply(stanza: MeTLImage) =
+          HavePropertyMatchResult(
+            stanza.source == expectedValue,
+            "source",
+            expectedValue,
+            stanza.source
+          )
+      }
+
+    def imageBytes(expectedValue: Box[Array[Byte]]) =
+      new HavePropertyMatcher[MeTLImage, Box[Array[Byte]]] {
+        def apply(stanza: MeTLImage) =
+          HavePropertyMatchResult(
+            compareBoxedArray(stanza.imageBytes, expectedValue),
+            "imageBytes",
+            expectedValue,
+            stanza.imageBytes
+          )
+      }
+
+    def pngBytes(expectedValue: Box[Array[Byte]]) =
+      new HavePropertyMatcher[MeTLImage, Box[Array[Byte]]] {
+        def apply(stanza: MeTLImage) =
+          HavePropertyMatchResult(
+            compareBoxedArray(stanza.pngBytes, expectedValue),
+            "pngBytes",
+            expectedValue,
+            stanza.pngBytes
+          )
+      }
+
+    def width(expectedValue: Double) =
+      new HavePropertyMatcher[MeTLImage, Double] {
+        def apply(stanza: MeTLImage) =
+          HavePropertyMatchResult(
+            stanza.width == expectedValue,
+            "width",
+            expectedValue,
+            stanza.width
+          )
+      }
+
+    def height(expectedValue: Double) =
+      new HavePropertyMatcher[MeTLImage, Double] {
+        def apply(stanza: MeTLImage) =
+          HavePropertyMatchResult(
+            stanza.height == expectedValue,
+            "height",
+            expectedValue,
+            stanza.height
+          )
+      }
+
+    def x(expectedValue: Double) =
+      new HavePropertyMatcher[MeTLImage, Double] {
+        def apply(stanza: MeTLImage) =
+          HavePropertyMatchResult(
+            stanza.x == expectedValue,
+            "x",
+            expectedValue,
+            stanza.x
+          )
+      }
+
+    def y(expectedValue: Double) =
+      new HavePropertyMatcher[MeTLImage, Double] {
+        def apply(stanza: MeTLImage) =
+          HavePropertyMatchResult(
+            stanza.y == expectedValue,
+            "y",
+            expectedValue,
+            stanza.y
+          )
+      }
+
+      def inkIds(expectedValue: Seq[String]) =
+        new HavePropertyMatcher[MeTLMoveDelta, Seq[String]] {
+          def apply(stanza: MeTLMoveDelta) =
+            HavePropertyMatchResult(
+              stanza.inkIds == expectedValue,
+              "inkIds",
+              expectedValue,
+              stanza.inkIds
+            )
+        }
+
+      def imageIds(expectedValue: Seq[String]) =
+        new HavePropertyMatcher[MeTLMoveDelta, Seq[String]] {
+          def apply(stanza: MeTLMoveDelta) =
+            HavePropertyMatchResult(
+              stanza.imageIds == expectedValue,
+              "imageIds",
+              expectedValue,
+              stanza.imageIds
+            )
+        }
+      def textIds(expectedValue: Seq[String]) =
+        new HavePropertyMatcher[MeTLMoveDelta, Seq[String]] {
+          def apply(stanza: MeTLMoveDelta) =
+            HavePropertyMatchResult(
+              stanza.textIds == expectedValue,
+              "textIds",
+              expectedValue,
+              stanza.textIds
+            )
+        }
+
+    def xTranslate(expectedValue: Double) =
+      new HavePropertyMatcher[MeTLMoveDelta, Double] {
+        def apply(stanza: MeTLMoveDelta) =
+          HavePropertyMatchResult(
+            stanza.xTranslate == expectedValue,
+            "xTranslate",
+            expectedValue,
+            stanza.xTranslate
+          )
+      }
+
+    def yTranslate(expectedValue: Double) =
+      new HavePropertyMatcher[MeTLMoveDelta, Double] {
+        def apply(stanza: MeTLMoveDelta) =
+          HavePropertyMatchResult(
+            stanza.yTranslate == expectedValue,
+            "yTranslate",
+            expectedValue,
+            stanza.yTranslate
+          )
+      }
+
+    def xScale(expectedValue: Double) =
+      new HavePropertyMatcher[MeTLMoveDelta, Double] {
+        def apply(stanza: MeTLMoveDelta) =
+          HavePropertyMatchResult(
+            stanza.xScale == expectedValue,
+            "xScale",
+            expectedValue,
+            stanza.xScale
+          )
+      }
+
+    def yScale(expectedValue: Double) =
+      new HavePropertyMatcher[MeTLMoveDelta, Double] {
+        def apply(stanza: MeTLMoveDelta) =
+          HavePropertyMatchResult(
+            stanza.yScale == expectedValue,
+            "yScale",
+            expectedValue,
+            stanza.yScale
+          )
+      }
+
+    def newPrivacy(expectedValue: Privacy) =
+      new HavePropertyMatcher[MeTLMoveDelta, Privacy] {
+        def apply(stanza: MeTLMoveDelta) =
+          HavePropertyMatchResult(
+            stanza.newPrivacy == expectedValue,
+            "newPrivacy",
+            expectedValue,
+            stanza.newPrivacy
+          )
+      }
+
+    def isDeleted(expectedValue: Boolean) =
+      new HavePropertyMatcher[MeTLMoveDelta, Boolean] {
+        def apply(stanza: MeTLMoveDelta) =
+          HavePropertyMatchResult(
+            stanza.isDeleted == expectedValue,
+            "isDeleted",
+            expectedValue,
+            stanza.isDeleted
+          )
+      }
+                          <yTranslate></yTranslate>
+                          <xScale></xScale>
+                          <yScale></yScale>
+                          <newPrivacy></newPrivacy>
+                          <isDeleted></isDeleted>
+}
+/*
+        result.tag should equal (expected.tag)
+        result.source should equal (expected.source)
+        result.imageBytes match {
+            case Full(image) => image should equal (expected.imageBytes.openOr(Array.empty[Byte]))
+            case _ => println("imageBytes does not match")
+        }
+        result.pngBytes should be (expected.pngBytes)
+        result.width should equal (expected.width)
+        result.height should equal (expected.height)
+        result.x should equal (expected.x)
+        result.y should equal (expected.y)
+*/
+
+class GenericXmlSerializerSuite extends FunSuite with MockitoSugar with BeforeAndAfter with ShouldMatchers with MeTLDataMatchers {
 
     var xmlSerializer: GenericXmlSerializer = _
 
@@ -383,4 +699,205 @@ class GenericXmlSerializerSuite extends FunSuite with MockitoSugar with BeforeAn
         assert(result === MeTLInk(ServerConfiguration.empty, "eecrole", -1L, 234235.234234, 233453.1498,List(Point(123.34,234,23)), 
             Color(255, 255, 0, 0), 40.0, false, "test", Privacy.PRIVATE, "4", "eecrole:223445834582"))
     }
+
+    test("extract metl text from xml") {
+
+        val content = <message>
+                        <textbox>
+                          <author>eecrole</author>
+                          <target>test</target>
+                          <privacy>private</privacy>
+                          <slide>4</slide>
+                          <identity>eecrole:223445834582</identity>
+                          <tag>eecrole:223445834582</tag>
+                          <caret>0</caret>
+                          <text>Hello World!</text>
+                          <style>Underline</style>
+                          <family>Helvetica</family>
+                          <weight>Bold</weight>
+                          <size>12.0</size>
+                          <decoration>Italics</decoration>
+                          <color>#ffff0000</color>
+                          <width>200</width>
+                          <height>100</height>
+                          <x>120</x>
+                          <y>300</y>
+                        </textbox>
+                      </message>
+
+        val result = xmlSerializer.toMeTLStanza(content)
+        assert(result === MeTLText(ServerConfiguration.empty, "eecrole", -1L, "Hello World!", 100.0, 200.0, 0, 120.0, 300.0, "eecrole:223445834582",
+            "Underline", "Helvetica", "Bold", 12.0, "Italics", "eecrole:223445834582", "test", Privacy.PRIVATE, "4", Color(255, 255, 0, 0)))
+    }
+
+    test("extract metl image from xml") {
+
+        val content = <message>
+                        <image>
+                          <author>eecrole</author>
+                          <target>test</target>
+                          <privacy>private</privacy>
+                          <slide>4</slide>
+                          <identity>metlImage</identity>
+                          <tag>eecrole:223445834582</tag>
+                          <source>http://test.metl.com/test/testimg23435.png</source>
+                          <width>200</width>
+                          <height>100</height>
+                          <x>120</x>
+                          <y>300</y>
+                        </image>
+                      </message>
+
+        val result = xmlSerializer.toMeTLStanza(content).asInstanceOf[MeTLImage]
+
+        result should have (
+            server (ServerConfiguration.empty),
+            author ("eecrole"),
+            timestamp (-1L),
+            tag ("eecrole:223445834582"),
+            source (Full("http://test.metl.com/test/testimg23435.png")),
+            imageBytes (Full(Array.empty[Byte])),
+            pngBytes (Empty),
+            width (200.0),
+            height (100.0),
+            x (120.0),
+            y (300.0),
+            target ("test"),
+            privacy (Privacy.PRIVATE),
+            slide ("4"),
+            identity ("metlImage")
+        )
+    }
+
+    test("extract metl dirty ink from xml") {
+
+        val content = <message>
+                        <dirtyInk>
+                          <author>eecrole</author>
+                          <target>test</target>
+                          <privacy>public</privacy>
+                          <slide>4</slide>
+                          <identity>metlDirtyInk</identity>
+                        </dirtyInk>
+                      </message>
+
+        val result = xmlSerializer.toMeTLStanza(content).asInstanceOf[MeTLDirtyInk]
+
+        result should have (
+            server (ServerConfiguration.empty),
+            author ("eecrole"),
+            timestamp (-1L),
+            target ("test"),
+            privacy (Privacy.PUBLIC),
+            slide ("4"),
+            identity ("metlDirtyInk")
+        )
+    }
+
+    test("extract metl dirty text from xml") {
+
+        val content = <message>
+                        <dirtyText>
+                          <author>eecrole</author>
+                          <target>test</target>
+                          <privacy>public</privacy>
+                          <slide>4</slide>
+                          <identity>metlDirtyText</identity>
+                        </dirtyText>
+                      </message>
+
+        val result = xmlSerializer.toMeTLStanza(content).asInstanceOf[MeTLDirtyText]
+
+        result should have (
+            server (ServerConfiguration.empty),
+            author ("eecrole"),
+            timestamp (-1L),
+            target ("test"),
+            privacy (Privacy.PUBLIC),
+            slide ("4"),
+            identity ("metlDirtyText")
+        )
+    }
+
+    test("extract metl dirty image from xml") {
+
+        val content = <message>
+                        <dirtyImage>
+                          <author>eecrole</author>
+                          <target>test</target>
+                          <privacy>public</privacy>
+                          <slide>4</slide>
+                          <identity>metlDirtyImage</identity>
+                        </dirtyImage>
+                      </message>
+
+        val result = xmlSerializer.toMeTLStanza(content).asInstanceOf[MeTLDirtyImage]
+
+        result should have (
+            server (ServerConfiguration.empty),
+            author ("eecrole"),
+            timestamp (-1L),
+            target ("test"),
+            privacy (Privacy.PUBLIC),
+            slide ("4"),
+            identity ("metlDirtyImage")
+        )
+    }
+
+    test("extract metl move delta from xml") {
+
+        val content = <message>
+                        <moveDelta>
+                          <author>eecrole</author>
+                          <target>test</target>
+                          <privacy>public</privacy>
+                          <slide>4</slide>
+                          <identity>metlMoveDelta</identity>
+                          <inkIds>
+                            <inkId>1</inkId>
+                            <inkId>2</inkId>
+                            <inkId>3</inkId>
+                          </inkIds>
+                          <textIds>
+                            <textId>4</textId>
+                            <textId>5</textId>
+                            <textId>6</textId>
+                          </textIds>
+                          <imageIds>
+                            <imageId>7</imageId>
+                            <imageId>8</imageId>
+                            <imageId>9</imageId>
+                          </imageIds>
+                          <xTranslate>142.4</xTranslate>
+                          <yTranslate>265.2</yTranslate>
+                          <xScale>2.0</xScale>
+                          <yScale>4.0</yScale>
+                          <newPrivacy>private</newPrivacy>
+                          <isDeleted>false</isDeleted>
+                        </moveDelta>
+                      </message>
+
+        val result = xmlSerializer.toMeTLStanza(content).asInstanceOf[MeTLMoveDelta]
+
+        result should have (
+            server (ServerConfiguration.empty),
+            author ("eecrole"),
+            timestamp (-1L),
+            target ("test"),
+            privacy (Privacy.PUBLIC),
+            slide ("4"),
+            identity ("metlMoveDelta"),
+            inkIds (Seq("1", "2", "3")),
+            textIds (Seq("4", "5", "6")),
+            imageIds (Seq("7", "8", "9")),
+            xTranslate (142.4),
+            yTranslate (265.2),
+            xScale (2.0),
+            yScale (4.0),
+            newPrivacy (Privacy.PRIVATE),
+            isDeleted (false)
+        )
+    }
+
+    test("extract metl quiz from xml") (pending)
 }
