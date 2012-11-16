56cf26bd8947696bb421d1a414b77f67a77bdfe0
David Hagan <david.hagan@monash.edu>
2012-11-16 04:13:40 +0000

    stage 1 complete - authentication works for providers who don't require that a username be inserted into the openId url.  Still reworking the solution for those which do.

--- OpenIdAuthentication/OpenIdAuthentication/src/main/scala/OpenIdAuthentication.scala
+++ OpenIdAuthentication/OpenIdAuthentication/src/main/scala/OpenIdAuthentication.scala
@@ -3,14 +3,39 @@ package com.metl.auth
 import _root_.org.openid4java.discovery._
 import _root_.org.openid4java.message.AuthRequest
 import _root_.org.openid4java.consumer._
+import net.liftweb._
+import net.liftweb.util._
 import net.liftweb.openid._
 import net.liftweb.common._
 import net.liftweb.http._
 import com.metl.cas._
 import net.liftweb.http.SHtml._
 import WellKnownAttributes._
+import net.liftweb.http.js.JsCmds._
+import scala.xml._
+import net.liftweb.http.provider.servlet._
 
-object isLoggedIn extends SessionVar[Boolean](false)
+case class OpenIdEndpoint(name:String,generateUrlWithUsername:String => String,imageSrc:String,usernameDescriptor:Box[String])
+
+object OpenIdEndpoint {
+	private var endpoints = Map.empty[String,OpenIdEndpoint]
+	def add(endpoint:OpenIdEndpoint):Unit = endpoints = endpoints.updated(endpoint.name.toLowerCase.trim,endpoint)
+	def remove(name:String):Unit = endpoints = endpoints.filter(e => e._1 != name.toLowerCase.trim)  
+	def find(name:String):Box[OpenIdEndpoint] = endpoints.get(name.toLowerCase.trim)
+	def empty:Unit = endpoints = Map.empty[String,OpenIdEndpoint]
+	def getAll:List[OpenIdEndpoint] = endpoints.values.toList
+	List(
+		OpenIdEndpoint("gmail",(s) => "https://www.google.com/accounts/o8/id","http://certainToFail.com",Empty),
+		OpenIdEndpoint("google",(s) => "https://www.google.com/profiles/~%s".format(s),"http://certainToFail.com",Full("google profile name")),
+		OpenIdEndpoint("yahoo",(s) => "https://me.yahoo.com","http://certainToFail.com",Empty),
+		OpenIdEndpoint("aol",(s) => "https://www.aol.com/","http://certainToFail.com",Empty),
+		OpenIdEndpoint("aolOpenId",(s) => "http://openid.aol.com/%s".format(s),"http://certainToFail.com",Full("aol screen name")),
+		OpenIdEndpoint("LiveJournal",(s) => "http://%s.livejournal.com/".format(s),"http://certainToFail.com",Full("LiveJournal username")),
+		OpenIdEndpoint("Wordpress",(s) => "http://%s.wordpress.com/".format(s),"http://certainToFail.com",Full("Worldpress account name")),
+		OpenIdEndpoint("Blogger",(s) => "http://%s.blogspot.com/".format(s),"http://certainToFail.com",Full("Blog name")),
+		OpenIdEndpoint("ClickPass",(s) => "http://clickpass.com/public/%s".format(s),"http://certainToFail.com",Full("ClickPass username"))
+	).foreach(e => add(e))	
+}
 
 object OpenIdAuthenticator {
   def attachOpenIdAuthenticator(enactor:OpenIdAuthenticator) = {
@@ -19,7 +44,13 @@ object OpenIdAuthenticator {
   }
 }
 
-class OpenIdAuthenticator(alreadyLoggedIn:()=>Boolean,onSuccess:CASStateData => Unit) extends OpenIdVendor {
+class OpenIdAuthenticator(incomingAlreadyLoggedIn:()=>Boolean,onSuccess:CASStateData => Unit) extends OpenIDVendor {
+	private object OriginalRequestPath extends SessionVar[Box[String]](Empty)
+	private object attemptInProgress extends SessionVar[Boolean](false)
+	private object isLoggedIn extends SessionVar[Boolean](false)
+
+	type UserType = Identifier
+	type ConsumerType = OpenIDConsumer[UserType]
   def ext(di: DiscoveryInformation, authReq: AuthRequest): Unit = {
     WellKnownEndpoints.findEndpoint(di) map {
       ep =>
@@ -29,17 +60,22 @@ class OpenIdAuthenticator(alreadyLoggedIn:()=>Boolean,onSuccess:CASStateData => 
         }
     }
   }
-  def alreadyLoggedIn = alreadyLoggedIn
+  def alreadyLoggedIn:Boolean = incomingAlreadyLoggedIn()
   override def createAConsumer = new OpenIDConsumer[UserType] {
     beforeAuth = Full(ext _)
   }
 
-  def postPf = {
-    case r @ Req(PathRoot :: ResponsePath :: _, "", _) =>
+  def postPf:LiftRules.DispatchPF = NamedPF("openIdPostLogin") {
+    //case r @ Req(PathRoot :: ResponsePath :: _, "", _) =>
+    case r @ Req("openid" :: "response" :: _, _, _) =>
       () => {
+				println("openId.postPf")
         for (req <- S.request;
              ret <- {
+							 println("generating ret from req: %s".format(req))
+							 attemptInProgress(false)
                val (id, res) = OpenIDObject.is.verifyResponse(req.request)
+							 println("verifiedResponse: %s".format((id,res)))
                id.map(i=>{
                  val attrs = WellKnownAttributes.attributeValues(res.getAuthResponse)
                  println("attrs")
@@ -58,7 +94,8 @@ class OpenIdAuthenticator(alreadyLoggedIn:()=>Boolean,onSuccess:CASStateData => 
                  case Full(f) => Full(f(id, Full(res), Empty))
                  case _ => {
                    postLogin(id, res)
-                   val rb = RedirectBackTo.is
+                   val rb = OriginalRequestPath.is
+									 println("redirecting back to: %s".format(rb))
                    Full(RedirectResponse(rb openOr "/", S responseCookies :_*))
                  }
                }
@@ -66,22 +103,83 @@ class OpenIdAuthenticator(alreadyLoggedIn:()=>Boolean,onSuccess:CASStateData => 
       }
   }
 
-  def prePf = {
-    case Req("openIdGoTo" :: choice :: paramName,_,_) => () => {
-      WellknownEndpoints.find(we => we.name == choice).map(we => {
-        OpenIDObject.is.authRequest(we.url, "/"+PathRoot+"/"+ResponsePath)
+  def prePf:LiftRules.DispatchPF = NamedPF("openIdPreLogin") {
+		case Req("openIdGoTo" :: choice :: usernameString :: Nil,_,_) => () => {
+			OpenIdEndpoint.find(choice).map(we => {
+				attemptInProgress(true)
+				OpenIDObject.is.authRequest(we.generateUrlWithUsername(usernameString),"/%s/%s".format(PathRoot,ResponsePath))
+			})
+		}
+    case r@Req("openIdGoTo" :: choice :: Nil,_,_) => () => {
+			attemptInProgress(false)
+			OpenIdEndpoint.find(choice).map(we => {
+				we.usernameDescriptor match {
+					case Full(argDesc) => {
+						val pathName = "/openIdGoTo/"+choice
+						var usernameString = ""
+					  S.session.map(sess => {
+							val xml = sess.processSurroundAndInclude("/openIdGoTo/"+choice,
+								<html xmlns="http://www.w3.org/1999/xhtml">
+									<head>
+										<script src="/static/javascript/stable/jquery-1.8.2.min.js"></script>
+									</head>
+									<body>
+										<div>
+											<div>You are attempting to use {we.name} to authenticate.</div>
+											<div>{we.name} requires a {argDesc}.</div>
+											<div>Please enter your {argDesc}.</div>
+												{
+												ajaxForm(Noop,
+													<div>
+													{
+														text("",(t:String) => {
+															usernameString = t
+														})
+													}
+													</div>
+													<div>
+													{
+														ajaxSubmit("submit",()=> RedirectTo("/openIdGoTo/%s/%s".format(we.name,usernameString)))
+													}
+													</div>
+												)
+											}
+										</div>
+											{
+												Script(net.liftweb.http.js.ScriptRenderer.ajaxScript)
+											}
+									</body>
+								</html>).theSeq.head
+							new XhtmlResponse(xml,Full("html"),Nil,Nil,200,true){
+								override def includeXmlVersion = false
+							}
+						}).openOr(RedirectResponse("/openIdGoTo/"+choice))
+					}
+					case _ => {
+						attemptInProgress(true)
+						OpenIDObject.is.authRequest(we.generateUrlWithUsername(""), "/"+PathRoot+"/"+ResponsePath)
+					}
+				}
       })
     }
-    case r:Req if (!isLoggedIn.is && !alreadyLoggedIn())=> {
-      RedirectBackTo(r.hostAndPath)
-      Full(InMemoryResponse(
-        <div>
-        {
-          WellknownEndpoints.map(we => {
-            a(<img alt={we.name} src={"http://certainToFail.com"}/>,() => RedirectTo("/openIdGoTo/"+we.name))
-          })
-        }</div>
-      ))
+    case r:Req if (!attemptInProgress.is && !isLoggedIn.is && !alreadyLoggedIn) => () => {
+      OriginalRequestPath(Full(r.hostAndPath))
+      Full(XhtmlResponse(
+				<html xmlns="http://www.w3.org/1999/xhtml">
+					<body>
+						<div>
+						{
+							OpenIdEndpoint.getAll.map(we => {
+								a(<span><div><img alt={we.name} src={we.imageSrc}/></div><div>{we.name}</div></span>,RedirectTo("/openIdGoTo/"+we.name))
+							})
+						}</div>
+					</body>
+				</html>
+      ,Empty,Nil,Nil,200,true))
     }
   }
+	override def logUserOut:Unit = {}
+	override def displayUser(id:OpenIdAuthenticator.this.UserType):NodeSeq = NodeSeq.Empty
+	override def currentUser:Box[OpenIdAuthenticator.this.UserType] = Empty
+	override def postLogin(id:Box[Identifier], res:VerificationResult):Unit = {}
 }
