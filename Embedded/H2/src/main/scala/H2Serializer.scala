3ad386c7b0645e2ba1240a73d7d57b5030ee83af
David Hagan <david.hagan@monash.edu>
2012-11-20 07:18:46 +0000

    revised separation of interfaces and work

---
+++ Embedded/H2/src/main/scala/H2Serializer.scala
@@ -0,0 +1,11 @@
+package com.metl.h2
+
+import com.metl.data._
+import com.metl.utils._
+
+import net.liftweb.common._
+import net.liftweb.util.Helpers._
+import Privacy._
+
+class H2Serializer(configName:String) extends Serializer{
+}
---
+++ Embedded/H2/src/main/scala/LocalH2Interface.scala
@@ -0,0 +1,27 @@
+package com.metl.h2
+
+import com.metl.data._
+import com.metl.utils._
+import com.metl.persisted._
+
+class H2Interface(configName:String,serializer:Serializer) extends PersistenceInterface{
+	//stanzas
+	def storeStanza(jid:String,stanza:MeTLStanza):Boolean = true
+	def getHistory(jid:String):History = History.empty
+
+	//conversations
+	def searchForConversation(query:String):List[Conversation] = List.empty[Conversation]
+	def conversationFor(slide:Int):Int = 0
+	def detailsOfConversation(jid:Int):Conversation = Conversation.empty
+	def createConversation(title:String,author:String):Conversation = Conversation.empty
+	def deleteConversation(jid:String):Conversation = Conversation.empty
+	def renameConversation(jid:String,newTitle:String):Conversation = Conversation.empty
+	def changePermissionsOfConversation(jid:String,newPermissions:Permissions):Conversation = Conversation.empty
+	def updateSubjectOfConversation(jid:String,newSubject:String):Conversation = Conversation.empty
+	def addSlideAtIndexOfConversation(jid:String,index:Int):Conversation = Conversation.empty
+	def reorderSlidesOfConversation(jid:String,newSlides:List[Slide]):Conversation = Conversation.empty
+
+	//resources
+	def getResource(identity:String):Array[Byte] = Array.empty[Byte]
+	def postResource(jid:String,userProposedId:String,data:Array[Byte]):String = ""
+}
