a13f05abbb2fda10d0797f5781ca9b262f3a4583
David Hagan <david.hagan@monash.edu>
2013-01-30 02:38:37 +0000

    updated the start script to use CAS

--- resilientMeTLLiftServer/start.sh
+++ resilientMeTLLiftServer/start.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
 #to connect to a single new metl server backend
-mvn jetty:run -Djetty.port=8088 -Dmetl.backend=standalone &
+mvn jetty:run -Djetty.port=8088 -Dmetl.backend=standalone -Drun.mode=production &
 
 #to use the internal H2 system
-#mvn jetty:run -Djetty.port=8088 -Dmetl.backend=external &
+#mvn jetty:run -Djetty.port=8088 -Dmetl.backend=external -Drun.mode=production &
 
 #to use the legacy monash MeTL2011 system
-#mvn jetty:run -Djetty.port=8088 &
+#mvn jetty:run -Djetty.port=8088 -Drun.mode=production &
