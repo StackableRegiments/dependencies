9d78ea4531bad2375adf3da6d438321a18557779
David Hagan <david.hagan@monash.edu>
2013-02-21 23:22:09 +0000

    added the stop script

--- resilientMeTLLiftServer/start.sh
+++ resilientMeTLLiftServer/start.sh
@@ -1,10 +1,15 @@
 #! /bin/sh
 
 #to connect to a single new metl server backend
-mvn jetty:run -Djetty.port=8088 -Dmetl.backend=standalone -Drun.mode=production &
+cd /srv/MeTLAuxiliaryServer
+ulimit -u 4096
+mvn jetty:run -Djetty.port=8080 -Dmetl.backend=standalone -Drun.mode=production 1>log/output.log 2>&1 & 
+
+#while debugging
+#mvn jetty:run -Djetty.port=8080 -Dmetl.backend=standalone -Drun.mode=production -Dmetl.stopwatch.minimumLog=10 -Dmetl.stopwatch.enabled=true 1>log/output.log 2>&1 &
 
 #to use the internal H2 system
-#mvn jetty:run -Djetty.port=8088 -Dmetl.backend=external -Drun.mode=production &
+#mvn jetty:run -Djetty.port=8080 -Dmetl.backend=external -Drun.mode=production 1>log/output.log 2>&1 &
 
 #to use the legacy monash MeTL2011 system
-#mvn jetty:run -Djetty.port=8088 -Drun.mode=production &
+#mvn jetty:run -Djetty.port=8080 1>log/output.log -Drun.mode=production 2>&1 &
