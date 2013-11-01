69a797439740b91ce3a1b02314226b525f792164
David Hagan <david.hagan@gmail.com>
2013-11-01 12:58:22 +1100

    working form submission system, but it's not quite right yet

--- LDAPAuthentication/LDAPAuthentication/src/main/scala/LDAPAuthentication.scala
+++ LDAPAuthentication/LDAPAuthentication/src/main/scala/LDAPAuthentication.scala
@@ -18,17 +18,30 @@ import net.liftweb.http.provider.HTTPCookie
 import org.apache.commons.io.IOUtils
 
 class LDAPAuthenticationSystem(mod:LDAPAuthenticator) extends LiftAuthenticationSystem {
-  override def dispatchTableItemFilter = (r) => !mod.checkWhetherAlreadyLoggedIn
+  override def dispatchTableItemFilter = (r) => false
+  protected def dispatchTableItemFilterInternal:Req=>Boolean = (r) => !mod.checkWhetherAlreadyLoggedIn
   override def dispatchTableItem(r:Req) = Full(mod.constructResponse(r))
-/*  LiftRules.dispatch.prepend {
-    case r@Req("ldapLogon" :: Nil,_,_) if dispatchTableItemFilter(r) && r.post_? => () => {
-      
+  LiftRules.dispatch.prepend {
+    case r@Req("ldapLogon" :: Nil,_,_) if dispatchTableItemFilterInternal(r) && r.post_? => () => {
+      println("received post: %s".format(r))
+      r.body.map(b => {
+        
+        val path = 
+        Full(RedirectResponse(path))
+      })
+      val username = S.param("username")
+      val password = S.param("password")
+      val path = S.param("path")      
+      mod.tryLogin(username,password)
+      Full(RedirectResponse(path))
+    }
+    case r:Req if dispatchTableItemFilterInternal(r) => () => {
+      dispatchTableItem(r)
     }
   }
-*/
 }
 
-class LDAPAuthenticator(loginPage:NodeSeq, usernameSelector:String, passwordSelector:String, submitButtonSelector:String, ldap: Option[IMeTLLDAP], alreadyLoggedIn:() => Boolean,onSuccess:(LiftAuthStateData) => Unit) extends LiftAuthenticator(alreadyLoggedIn,onSuccess) {
+class LDAPAuthenticator(loginPage:NodeSeq, formSelector:String, usernameSelector:String, passwordSelector:String, ldap: Option[IMeTLLDAP], alreadyLoggedIn:() => Boolean,onSuccess:(LiftAuthStateData) => Unit) extends LiftAuthenticator(alreadyLoggedIn,onSuccess) {
 
   private val getLDAP: IMeTLLDAP = ldap.getOrElse(DisconnectedLDAP)
 
@@ -36,66 +49,33 @@ class LDAPAuthenticator(loginPage:NodeSeq, usernameSelector:String, passwordSele
 
   def tryLogin(username:String,password:String):Boolean = {
     println("LDAP.tryLogin")
-    onSuccess(LiftAuthStateDataForbidden)
+    onSuccess(LiftAuthStateData(true,username,List.empty[Tuple2[String,String]],List.empty[Tuple2[String,String]]))
     true
   }
  
-  val defaultNodeSeq:NodeSeq = {
-    <html xmlns:lift="http://liftweb.net/" xmlns="http://www.w3.org/1999/xhtml">
-      <head>
-        <meta content="text/html; charset=UTF-8" http-equiv="content-type" />
-        <title>LDAP Login</title>
-      </head>
-      <body><div>Error</div><div>This system does not have a valid authentication page set up.  It is not possible to log into this system.</div></body>
-    </html>
-  }
-
   override def constructResponse(req:Req) = Stopwatch.time("LDAPAuthenticator.constructReq",() => {
+      println("constructing response: %s".format(req))
       var username = ""
       var password = ""
-      val withAddedScript = 
-        <html xmlns="http://www.w3.org/1999/xhtml" xmlns:lift="http://liftweb.net/">
-          <head>
-            <!--<script id="jquery" src="/classpath/jquery.js" type="text/javascript"></script>-->
-            <script id="jquery" src="/static/js/jquery-1.7.2.min.js" type="text/javascript"></script>
-            <script id="json" src="/classpath/json.js" type="text/javascript"></script>
-          </head>
-          <body>
-            {loginPage}
-            {Script(ScriptRenderer.ajaxScript)}
-            <div data-lift="logonPage"></div>
-          </body>
-        </html>
-      val loginPageNode = 
+      val loginPageNode =
         (
-          usernameSelector #> ajaxText(username,(t) => {
-            username = t
-          }) &
-          passwordSelector #> ajaxText(password,(t) => {
-            password = t
-          },("type","password")) &
-          submitButtonSelector #> ajaxButton("login",() => {
-            if (tryLogin(username,password)){
-              RedirectTo(req.uri)
-            } else {
-              Alert("login error") 
-            }
-          })
-        ).apply(withAddedScript)
-
-      val output = for (
-        session <- S.session;
-        response <- session.processTemplate(Full(session.processSurroundAndInclude("logonPage",loginPageNode)),req,req.path,200)
-      ) yield {
-        response
-      }
-      output.openOr(PlainTextResponse("serverside authentication error",500))   
-/*      LiftRules.convertResponse(
+          formSelector #> {(formNode:NodeSeq) => {
+            (
+              "%s [method]".format(formSelector) #> "POST" &
+              "%s [action]".format(formSelector) #> "/ldapLogon" &
+// these next two lines aren't working, and I'm not sure why not
+              "%s [name]".format(usernameSelector) #> "username" &
+              "%s [name]".format(passwordSelector) #> "password" &
+              "%s *+".format(formSelector) #> <input type="hidden" name="path" value={req.uri}></input> &
+              "%s *+".format(formSelector) #> <input type="submit" value="Logon"></input>
+            ).apply(formNode) 
+          }} 
+        ).apply(loginPage)
+      LiftRules.convertResponse(
         (loginPageNode,200),
         S.getHeaders(LiftRules.defaultHeaders((loginPageNode,req))),
         S.responseCookies,
         req
       )
-*/
   })
 }
