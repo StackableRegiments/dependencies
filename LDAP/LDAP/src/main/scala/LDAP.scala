e6d7885e974d88e3d454f769e006d4cd04c1aece
David Hagan <david.hagan@gmail.com>
2014-03-19 17:08:14 +1100

    updated LDAP.scala to include groups, a full query, and also a minor refactor of the namingEnumeration handling

--- LDAP/LDAP/src/main/scala/LDAP.scala
+++ LDAP/LDAP/src/main/scala/LDAP.scala
@@ -8,16 +8,24 @@ import javax.naming.directory._
 import net.liftweb.common._
 import net.liftweb.util.Helpers._
 
+case class GroupDefinition(groupName:String,owners:List[String],members:List[String])
+
 trait IMeTLLDAP {
   def authenticate(username:String,password:String):Option[Boolean]
+  def fullQuery(attrName:String,attrValue:String):List[Seq[Tuple2[String,String]]]
 	def simpleQuery(attrName:String,attrValue:String,returnType:String):List[String] 
   def getValuePairs(attrName:String,attrValue:String,returnTypes:List[String]):List[Tuple2[String,Seq[Tuple2[String,String]]]]
+  def getGroupDefinition(groupName:String):Option[GroupDefinition]
+  def getGroupsFor(username:String):List[GroupDefinition] 
 }
 
 object DisconnectedLDAP extends IMeTLLDAP {
   override def authenticate(username:String,password:String) = None
+  override def fullQuery(attrName:String,attrValue:String):List[Seq[Tuple2[String,String]]] = List.empty[Seq[Tuple2[String,String]]]
  	override def simpleQuery(attrName:String,attrValue:String,returnType:String):List[String] = List.empty[String]
   override def getValuePairs(attrName:String,attrValue:String,returnTypes:List[String]):List[Tuple2[String,Seq[Tuple2[String,String]]]] = List.empty[Tuple2[String,Seq[Tuple2[String,String]]]]
+  def getGroupDefinition(groupName:String):Option[GroupDefinition] = None
+  override def getGroupsFor(username:String):List[GroupDefinition] = List.empty[GroupDefinition]
 }
 
 trait LDAPSearch {
@@ -102,6 +110,24 @@ class LDAP(env : { val ldap: LDAPService }) extends IMeTLLDAP {
     })
     output
   })
+  override def fullQuery(attrName:String,attrValue:String):List[Seq[Tuple2[String,String]]] = Stopwatch.time("LDAP.fullQuery", () => {
+  	var output = List.empty[Seq[Tuple2[String,String]]]
+		val func = (n:List[SearchResult]) => {
+			output = n.map(ne => {
+        try {
+          Some(listFromNamingEnum[Attribute,Attribute](ne.getAttributes().getAll.asInstanceOf[NamingEnumeration[Attribute]],a => a).map(a => {
+
+            val fullDn = "%s, %s".format(ne.getName,env.ldap.searchService.bindBase)
+            ("dn",fullDn) :: namingEnumerationToSeq(a.getID,a.getAll).toList 
+          }).flatten) 
+        } catch {
+          case e:Throwable => None
+        }
+      }).flatten
+		}
+		env.ldap.withLDAP("(%s=%s)".format(attrName,attrValue),func)
+		output
+	})
 	override def simpleQuery(attrName:String,attrValue:String,returnType:String):List[String] = Stopwatch.time("LDAP.simpleQuery", () => {
 		var output = List.empty[String]
 		val func = (n:List[SearchResult]) => {
@@ -115,17 +141,60 @@ class LDAP(env : { val ldap: LDAPService }) extends IMeTLLDAP {
 		val func = (n:List[SearchResult]) => {
       output = n.map(net => {
         val ne = net.asInstanceOf[SearchResult]
-        Tuple2(ne.getName,returnTypes.map(group => (group,ne.getAttributes().get(group))).filter{case (name,attr) => attr != null}.map{case (name:String,attrib:Attribute) => namingEnumerationToSeq(name,attrib.getAll)}.flatten)
+        Tuple2(ne.getName,returnTypes.map(group => getSeqOfValuesFromSearchResult(ne,group)).flatten)
 			})
 		}
 		env.ldap.withLDAP("(%s=%s)".format(attrName,attrValue),func)
 		output
   })	
-  protected def namingEnumerationToSeq(name:String,namingEnumeration:NamingEnumeration[_]):Seq[(String,String)]= Stopwatch.time("LDAP.namingEnumerationToSeq", () => {
-    var mySeq = List.empty[(String,String)] 
-    while (namingEnumeration.hasMoreElements()) {
-        Option(namingEnumeration.nextElement).foreach( v => mySeq = (name, v.toString) :: mySeq )
+  protected def listFromNamingEnum[A,B](namingEnumeration:NamingEnumeration[A],transformer:A=>B):List[B] = Stopwatch.time("LDAP.listFromNamingEnum", () => {
+    var mySeq = List.empty[B]
+    while (namingEnumeration.hasMoreElements()){
+      Option(namingEnumeration.nextElement).foreach(v => mySeq = mySeq ::: List(transformer(v)))
     }
     mySeq
+  })
+  protected def namingEnumerationToSeq[A](name:String,namingEnumeration:NamingEnumeration[A]):Seq[(String,String)]= Stopwatch.time("LDAP.namingEnumerationToSeq", () => {
+    listFromNamingEnum[A,String](namingEnumeration,n => n.toString).map(n => (name,n))
+  })
+  protected def getSeqOfValuesFromSearchResult(in:SearchResult,requestedValue:String):Seq[Tuple2[String,String]] = {
+    List(in.getAttributes().get(requestedValue)).filter{case attr => attr != null}.map{case attrib:Attribute => namingEnumerationToSeq(requestedValue,attrib.getAll)}.flatten
+  }
+  protected val ownerKey = "owner"
+  protected val memberKey = "uniquemember"
+  override def getGroupDefinition(groupName:String):Option[GroupDefinition] = Stopwatch.time("LDAP.getGroupDefinition",() => {
+    var output:Option[GroupDefinition] = None
+    val func = (n:List[SearchResult]) => {
+      output = n.headOption.map(ho => {
+        val owners = getSeqOfValuesFromSearchResult(ho,ownerKey).filter(i => i._1 == ownerKey).map(i => i._2).toList
+        val members = getSeqOfValuesFromSearchResult(ho,memberKey).filter(i => i._1 == memberKey).map(i => i._2).toList
+        GroupDefinition(groupName,owners,members)
+      })
+    }
+    env.ldap.withLDAP("(cn=%s)".format(groupName),func)
+    output
+  })
+  override def getGroupsFor(username:String):List[GroupDefinition] = Stopwatch.time("LDAP.getGroupsFor", () => {
+    fullQuery("uid",username).map(sr => {
+      val dn = sr.find(i => i._1 == "dn").map(i => i._2)
+      dn.map(getGroupsForDn(_))
+    }).flatten.flatten
+  })
+  protected def getGroupsForDn(dn:String):List[GroupDefinition] = Stopwatch.time("LDAP.getGroupsForDn", () => {
+    val filter = "(&((objectclass=groupOfUniqueNames)(|((%s=%s)(%s=%s)))))".format(ownerKey,dn,memberKey,dn)
+    getGroupsForDnWithFilter(dn,filter)
+  })
+  protected def getGroupsForDnWithFilter(dn:String,filterString:String):List[GroupDefinition] = Stopwatch.time("LDAP.getGroupsForDnWithFilter", () => {
+    var output = List.empty[GroupDefinition]
+    val func = (n:List[SearchResult]) => {
+      output = n.map(ne => {
+        val groupName = getSeqOfValuesFromSearchResult(ne,"cn").map(i => i._2).head
+        val owners = getSeqOfValuesFromSearchResult(ne,ownerKey).filter(i => i._1 == ownerKey).map(i => i._2).toList
+        val members = getSeqOfValuesFromSearchResult(ne,memberKey).filter(i => i._1 == memberKey).map(i => i._2).toList
+        GroupDefinition(groupName,owners,members)
+      }).toList
+    }
+    env.ldap.withLDAP(filterString,func)
+    output
   })
 }
